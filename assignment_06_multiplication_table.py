c694a16ab750c865674b96d48125d0ae62d7dd82
Kweku-AO <koamoakwa@st.ug.edu.gh>
2026-07-28 18:28:48 +0000

    Update assignment_06_multiplication_table.py

--- assignment_06_multiplication_table.py
+++ assignment_06_multiplication_table.py
@@ -55,3 +55,32 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+def print_single_table(num):
+    print(f"\nMultiplication Table for {num}:")
+    for i in range(1, 13):
+        print(f"{num:<2} x  {i:<2} =  {num * i}")
+
+def print_multiple_tables(n):
+    
+    for current_num in range(1, n + 1):
+        print_single_table(current_num)
+        if current_num < n:
+            print("-" * 27)
+
+
+if _name_ == "_main_":
+    print("--- PART A: Single Table ---")
+    single_num = int(input("Enter a number: "))
+    
+    if single_num <= 0:
+        print("Error: Value must be a positive integer.")
+    else:
+        print_single_table(single_num)
+        
+    print("\n--- PART B: Tables from 1 to N ---")
+    max_range = int(input("Enter a number N: "))
+    
+    if max_range <= 0:
+        print("Error: N must be a positive integer.")
+    else:
+        print_multiple_tables(max_range)
